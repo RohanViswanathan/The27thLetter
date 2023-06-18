b6f4e0efd17843ba47b650cd5584243fc0a8a0bb
Derek Xu <xzrderek@berkeley.edu>
2023-06-18 05:57:56 -0700

    adding image hover

--- update_website.py
+++ update_website.py
@@ -4,6 +4,7 @@ import time
 from dotenv import load_dotenv
 import tkinter as tk
 from pywebcopy import save_webpage
+import replicate
 
 load_dotenv()
 
@@ -56,19 +57,43 @@ url = url.replace('~', '')
 split_url = url.split('/')
 recombined_url = '/'.join(split_url[2:-1])
 
-filename = os.path.join(os.path.expanduser("~"), "Downloads", project_name, recombined_url, "index.html")
+# filename = os.path.join(os.path.expanduser("~"), "Downloads", project_name, recombined_url, "index.html")
+filename = os.path.join(os.path.expanduser("~"), "Downloads", "website-short.html")
 with open(filename, 'r', encoding='utf-8') as file:
     html_content = file.read()
 
-prompt = "Modify the following html code to be more accessible for the visually impaired, by making the font sizes bigger, fixing spacing issues by adding more space between visual elements, and increasing the contrast of images. Also make red and green elements more conducive to people that are color-blind, by changing their color and style. Return just the revised html code."
+# Image Captioning
+
+directory = os.path.join(os.path.expanduser("~"), "Downloads", project_name, recombined_url)
+
+captions = {}
+
+for file in os.listdir(directory):
+    f = os.path.join(directory, file)
+    if f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.png') or f.endswith('gif'):
+        output = replicate.run(
+            "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
+            input={"image": open(f, "rb")}
+        )
+        captions[file] = output
+
+print(captions)
+
+prompt = '''Modify the following html code to be more accessible for the visually impaired, by making the font sizes bigger, fixing spacing issues by 
+adding more space between visual elements. Also make red and green elements more conducive to people that are 
+color-blind, by changing their color and style. You will also replace HTML tags with the tag "img" with code that will allow me to hover over images to 
+show corresponding text. For each of these HTML "img" tags, it will contain a "src" that has a file, and you will look for the corresponding text in a 
+dictionary I will provide. I will provide the dictionary and HTMl code now. Return just the revised html code.'''
 
 start_time = time.time()
 
 print("Starting query...")
 
+print(prompt + "Dictionary: " + str(captions) +  ". HTML: " + html_content)
+
 response = openai.ChatCompletion.create(
     model = 'gpt-4', 
-    messages=[{"role": "user", "content": prompt + html_content}],
+    messages=[{"role": "user", "content": prompt + "Dictionary: " + str(captions) +  ". HTML: " + html_content}],
     )
 
 end_time = time.time()
