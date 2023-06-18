62d5d10157d168ba44dbd90231314adfb7797bee
Rohan Viswanathan <rohanvis@berkeley.edu>
2023-06-18 04:55:37 -0700

    Test full file with breakdown and multiple queries

--- update_website.py
+++ update_website.py
@@ -58,23 +58,51 @@ recombined_url = '/'.join(split_url[2:-1])
 
 filename = os.path.join(os.path.expanduser("~"), "Downloads", project_name, recombined_url, "index.html")
 with open(filename, 'r', encoding='utf-8') as file:
-    html_content = file.read()
+    html_content = file.readlines()
 
-prompt = "Modify the following html code to be more accessible for the visually impaired, by making the font sizes bigger, fixing spacing issues by adding more space between visual elements, and increasing the contrast of images. Return just the revised html code."
+prompt = "Modify the following HTML code to be more accessible for the visually impaired, by making the font sizes bigger, fixing spacing issues by adding more space between visual elements, and increasing the contrast of images. Also make colors more accessible for people who are colorblind. Rearrage sections and add padding to make the website look nicer, if needed, without removing information. Return just the revised HTML code."
 
 start_time = time.time()
 
 print("Starting query...")
 
-response = openai.ChatCompletion.create(
-    model = 'gpt-4', 
-    messages=[{"role": "user", "content": prompt + html_content}],
+revised_html_parts = []
+current_part = ""
+character_count = 0
+max_character_count = 7000
+
+for line in html_content:
+    line = line.strip()
+    line_length = len(line)
+
+    # Check if adding the line will exceed the maximum character count
+    if character_count + line_length > max_character_count:
+        revised_html_parts.append(current_part)
+        current_part = line
+        character_count = line_length
+    else:
+        current_part += line
+        character_count += line_length
+
+# Append the last part
+if current_part:
+    revised_html_parts.append(current_part)
+
+# Process each part with the OpenAI API
+revised_html = ""
+
+i = 0
+for part in revised_html_parts:
+    print(i, "part ...")
+    i += 1
+    response = openai.ChatCompletion.create(
+        model='gpt-4',
+        messages=[{"role": "user", "content": prompt + part}],
     )
+    revised_html += response.choices[0].message.content
 
 end_time = time.time()
 duration = end_time - start_time
-
-revised_html = response.choices[0].message.content
 
 print("Finished query...")
 
