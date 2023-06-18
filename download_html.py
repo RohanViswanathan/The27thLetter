2982f5c24da22f94067738f993de3079d15141ea
Derek Xu <xzrderek@berkeley.edu>
2023-06-18 01:05:12 -0700

    webpage downloads, images/media included

--- download_html.py
+++ download_html.py
@@ -1,16 +1,23 @@
 import tkinter as tk
 import urllib.request
 import os
+from pywebcopy import save_webpage
 
 def download_html():
     url = url_entry.get()
     if url:
         try:
-            response = urllib.request.urlopen(url)
-            html = response.read().decode('utf-8')
-            filename = os.path.join(os.path.expanduser("~"), "Downloads", "website.html")
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(html)
+            folder_name = os.path.join(os.path.expanduser("~"), "Downloads")
+            save_webpage(
+                url=url,
+                project_folder=folder_name,
+                project_name="my_site",
+                bypass_robots=True,
+                debug=True,
+                open_in_browser=True,
+                delay=None,
+                threaded=False,
+            )
             print("HTML source code downloaded successfully!")
         except Exception as e:
             print("An error occurred while downloading the HTML source code:", str(e))
